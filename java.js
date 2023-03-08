39a8f96c4956b266fc3c680b539876eb90b19f00
Tayyab Sajjad <devtayyabsajjad@gmail.com>
2023-03-09 01:07:54 +0500

    First Version

---
+++ java.js
@@ -0,0 +1,75 @@
+// 1
+// let myVar = 5;
+// console.log(myVar);
+
+// 2
+// let myString = "Hello World!";
+// alert(myString);
+
+// 3
+// let num1 = 5;
+// let num2 = 10;
+// let userNum = parseInt(prompt("Enter a Number"));
+// let sum = num1 + num2 + userNum;
+// console.log(sum);
+
+// 4 
+// let myBoolean = true;
+// if(myBoolean == true){
+//     console.log("it's true!");
+// }
+// else{
+//     console.log("it's false!");
+// }
+
+// 5
+// let num = parseInt(prompt("Enter a Number"));
+// if(num%2 == 0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+
+// 6
+// let num1 = "Hello " + "World!";
+// console.log(num1);
+
+// 7
+// let num1 = 2;
+// let num2 = 4;
+// ans = num1 * num2;
+// console.log(ans);
+
+// 8
+// let userInput = prompt("Enter password");
+// if(userInput === "password"){
+//     alert("Access granted");
+// }
+// else{
+//     alert("Access denied");
+// }
+
+// 9 
+// let num1 = parseInt(prompt("Enter first number"));
+// let num2 = parseInt(prompt("Enter second number"));
+// let num3 = parseInt(prompt("Enter third number"));
+// if(num1 > num2 && num1 > num3){
+//     alert(num1);
+// }
+// else if(num2 > num1 && num2 > num3){
+//     alert(num2);
+// }
+// else{
+//     alert(num3);
+// }
+
+// 10
+// let userInput1 = String(prompt("Enter message"));
+// let userInput2 = String(prompt("Enter message"));
+// if(userInput1 == userInput2){
+//     alert("The strings are equal");
+// }
+// else{
+//     alert("The strings are not equal");
+// }
